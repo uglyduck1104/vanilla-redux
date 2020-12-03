9d8fa11daecd4cd988ec0436e217dca54a2090d9
YongJun Lee <mlionlove54@gmail.com>
2020-12-03 20:53:03 +0900

    #1 PURE REDUX: COUNTER - Recap Refactor

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -6,14 +6,18 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS"
+
 const countModifier = (count = 0, action) => {
-    if (action.type === "ADD") {
-      return count + 1;
-    } else if(action.type === "MINUS"){
-      return count - 1;
-    } else {
+  switch (action.type){
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count -1
+    defualt:
       return count;
-    }
+  }
 };
 
 const countStore = createStore(countModifier);
@@ -25,10 +29,10 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({type: "ADD"});
+  countStore.dispatch({type: ADD});
 }
 const handleMinus = () => {
-  countStore.dispatch({type:"MINUS"});
+  countStore.dispatch({type: MINUS});
 }
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);

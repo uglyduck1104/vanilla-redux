a8333164e10d8bdca7676d85381cc5b5448b5a56
YongJun Lee <mlionlove54@gmail.com>
2020-12-18 23:03:16 +0900

    #3 REACT REDUX - Connecting the Store

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./component/App";
+import store from "./component/store";
 
-ReactDOM.render(<App></App>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}> 
+    <App />
+  </Provider>, 
+  document.getElementById("root")
+);

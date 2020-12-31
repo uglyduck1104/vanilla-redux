1be75ac5ffd43d00dd424c3bbb3f25bf6d296131
YongJun Lee <mlionlove54@gmail.com>
2020-12-31 20:46:04 +0900

    #4 REDUX TOOLKIT - createReducer

--- vanilla-redux/src/store.js
+++ vanilla-redux/src/store.js
@@ -1,19 +1,30 @@
 import { createStore } from "redux";
-import {createAction} from "@reduxjs/toolkit";
+import {createAction, createReducer} from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch(action.type){
-    case addToDo.type:
-      return [{ text:action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter(toDo => toDo.id !== action.payload);
-    default:
-      return state;
+// const reducer = (state = [], action) => {
+//   switch(action.type){
+//     case addToDo.type:
+//       return [{ text:action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter(toDo => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer([], {
+  // push return mutate!!
+  [addToDo]: (state, action) => {
+    state.push({ text:action.payload, id: Date.now() });
+  },
+  // filter return new array
+  [deleteToDo]: (state, action) => {
+    state.filter(toDo => toDo.id !== action.payload);
   }
-};
+});
 
 const store = createStore(reducer);
 

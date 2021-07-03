caa4ef6f0e834f98dc31d48399913f9431116ea4
YongJun Lee <mlionlove54@gmail.com>
2021-07-03 18:32:56 +0900

    commit test

---
+++ src/routes/Home.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import ToDo from '../component/ToDo';
+import { add } from "../store";
+
+function Home({toDos, addToDo}){
+  const [text, setText] = useState("");
+  function onChange(e){
+    setText(e.target.value);
+  }
+  function onSubmit(e){
+    e.preventDefault();
+    addToDo(text);
+    setText("");
+  }
+  return (
+    <>
+      <h1>To do</h1>
+      <form onSubmit={onSubmit}>
+          <input type="text" value={text} onChange={onChange}/>
+          <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map(toDo => (
+          <ToDo {...toDo} key={toDo.id}/>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+function mapStateToProps(state){
+  return {toDos: state}
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    addToDo: text => dispatch(add(text))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Home);

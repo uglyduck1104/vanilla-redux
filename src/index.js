6b5592a6260f8230d78d331b709f05d1620ec2a0
YongJun Lee <mlionlove54@gmail.com>
2021-07-03 20:49:38 +0900

    Add Subscriptions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 const countModifier = (count = 0, action) => {
   if (action.type === "ADD"){
     return count + 1;
@@ -16,6 +18,17 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+countStore.subscribe(onChange);
 
-console.log(countStore.getState());
+const handleAdd = () =>{
+  countStore.dispatch({ type: "ADD" });
+}
+
+const handleMinus = () =>{
+  countStore.dispatch({ type: "MINUS" });
+}
+add.addEventListener("click", handleAdd)
+minus.addEventListener("click", handleMinus)

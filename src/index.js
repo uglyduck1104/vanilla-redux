db93299f23d140d490dd1fba99d75dd8fde7fe5c
YongJun Lee <mlionlove54@gmail.com>
2021-07-03 20:37:41 +0900

    Action concept flow

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD"){
+    return count + 1;
+  }else if(action.type === "MINUS"){
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+
 console.log(countStore.getState());
